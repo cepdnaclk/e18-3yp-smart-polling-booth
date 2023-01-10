24bf418008ba929a00739d4bbca10b0c415394bc
dhananjaya weerasinghe <e18073@eng.pdn.ac.lk>
2023-01-10 14:38:55 +0530

    edit backend yml

--- code/backend/index.js
+++ code/backend/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.get("/", async (req, res) => {
   try {
-    console.log("Hi Hello");
+    console.log("HiHello");
     var query = {};
 
     query.currentVoteCount = await Votes.estimatedDocumentCount();
