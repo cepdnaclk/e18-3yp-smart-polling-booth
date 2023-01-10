41dd9e43ea63da7bceb9c0db2f2b5eb269214187
dhananjaya weerasinghe <e18073@eng.pdn.ac.lk>
2023-01-10 14:35:35 +0530

    edit dashboard get

--- code/backend/index.js
+++ code/backend/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.get("/", async (req, res) => {
   try {
-    console.log("Hi Hello");
+    console.log("called");
     var query = {};
 
     query.currentVoteCount = await Votes.estimatedDocumentCount();
