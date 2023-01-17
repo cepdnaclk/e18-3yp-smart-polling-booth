0589b08769532bb0051f55b83d0c51f13166a854
KushanManahara <e18214@eng.pdn.ac.lk>
2023-01-18 03:06:05 +0530

    voter details from the server were saved in voterDetails.py file

---
+++ code/Hardware/Project/voterDetails.py
@@ -0,0 +1,17 @@
+voters = [
+    {
+        "_id" : "_id:11111111",
+        "fname" : "Kushan",
+        "nic" : "123123123V",
+        "divisionID" : "_id:123456789",
+        "fingerprint" : "001-_id:123456789",
+        "faceImg" : "001-_id:123456789"
+    },
+    {   "_id" : "_id:11111113",
+        "fname" : "Dhananjaya",
+        "nic" : "123123125V",
+        "divisionID" : "_id:123456780",
+        "fingerprint" : "003-_id:123456780",
+        "faceImg" : "002-_id:123456780"
+    }
+]
